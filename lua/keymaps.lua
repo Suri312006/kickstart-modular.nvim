92e5788837d00a4d9c36ce53489cf069dca33184
Surendra <suri312006@gmail.com>
2024-01-08 11:24:14 -0800

    uhhh fixed some keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -66,18 +66,19 @@ vim.keymap.set("n", "Q", "<nop>")
 --easy format
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
+-- IDK WHAT THESE FUCKING DO LOLLLLL
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+--
 --replace the workd youre on
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- make scripts executabe from vim
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-
-
+--remap window movement
+-- vim.keymap.set("n", "qk", "<C-w>k")
 -- vim: ts=2 sts=2 sw=2 et
 
