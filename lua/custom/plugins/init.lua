435aeab3ed636a692ba8e9d81f7fb8d87ea7b10d
suri312006 <suri312006@gmail.com>
2024-04-03 18:14:45 -0700

    trying to get vimtex working

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -107,7 +107,7 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-      require("refactoring").setup()
+    require("refactoring").setup(config)
     end,
   },
 {
