02c17630dd3abf79a89667a433501d489241a33e
suri312006 <suri312006@gmail.com>
2024-04-27 08:54:58 -0700

    wahooooo new harpoon keybinds

--- lua/custom/configs/harpoon.lua
+++ lua/custom/configs/harpoon.lua
@@ -35,20 +35,20 @@ harpoon:extend({
   end,
 })
 -- this part doesnt let you move around or remove snippers
-vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+vim.keymap.set("n", "<C-h>", function() toggle_telescope(harpoon:list()) end,
     { desc = "Open harpoon window" })
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<A-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<A-5>", function() harpoon:list():select(5) end)
-vim.keymap.set("n", "<A-6>", function() harpoon:list():select(6) end)
-vim.keymap.set("n", "<A-7>", function() harpoon:list():select(7) end)
-vim.keymap.set("n", "<A-8>", function() harpoon:list():select(8) end)
-vim.keymap.set("n", "<A-9>", function() harpoon:list():select(9) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-e>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-i>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-o>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end)
+vim.keymap.set("n", "<C-6>", function() harpoon:list():select(6) end)
+vim.keymap.set("n", "<C-7>", function() harpoon:list():select(7) end)
+vim.keymap.set("n", "<C-8>", function() harpoon:list():select(8) end)
+vim.keymap.set("n", "<C-9>", function() harpoon:list():select(9) end)
 -- vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
 -- vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
 -- vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
